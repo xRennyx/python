0d4fdddf6c92ecdf0703689a5d2f17d52fcf3e29
xRennyx <150335643+xRennyx@users.noreply.github.com>
2026-01-09 09:55:02 +0100

    Update Client.py

--- Socket/Client.py
+++ Socket/Client.py
@@ -1,16 +1,16 @@
 import socket
 
 def start_client():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', 12345))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Crea un socket TCP
+    client_socket.connect(('localhost', 12345)) # Connette al server
 
     message = "Ciao dal client!"
-    client_socket.sendall(message.encode())
+    client_socket.sendall(message.encode()) # Invia il messaggio al server
 
-    data = client_socket.recv(1024).decode()
+    data = client_socket.recv(1024).decode() # Riceve la risposta dal server
     print(f"Risposta dal server: {data}")
 
-    client_socket.close()
+    client_socket.close() # Chiude la connessione
 
-if __name__ == "__main__":
-    start_client
+if __name__ == "__main__": 
+    start_client # Avvia il client
