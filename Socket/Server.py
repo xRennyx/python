d371dc9b249af058dbbfc616dec1407ba78abaa0
xRennyx <150335643+xRennyx@users.noreply.github.com>
2026-01-09 09:55:28 +0100

    Update Server.py

--- Socket/Server.py
+++ Socket/Server.py
@@ -1,23 +1,23 @@
 import socket
 
 def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Crea un socket TCP
     server_socket.bind(('127.0.0.1', 12345))  # Indirizzo e porta
     server_socket.listen(5)
     print("Server in ascolto sulla porta 12345...")
     while(1):
-        conn, addr = server_socket.accept()
+        conn, addr = server_socket.accept() # Accetta una connessione
         print(f"Connessione stabilita con {addr}")
 
-        data = conn.recv(1024).decode()
+        data = conn.recv(1024).decode() # Riceve il messaggio dal client
         print(f"Messaggio ricevuto: {data}")
 
-        response = "Ciao dal server!"
-        conn.send(response.encode())
+        response = "Ciao dal server!" # Risposta del server
+        conn.send(response.encode()) # Invia la risposta al client
 
-        conn.close()
+        conn.close() # Chiude la connessione
     
-    server_socket.close()
+    server_socket.close() # Chiude il socket del server                     
 
 if __name__ == "__main__":
-    start_server()
+    start_server() # Avvia il server
